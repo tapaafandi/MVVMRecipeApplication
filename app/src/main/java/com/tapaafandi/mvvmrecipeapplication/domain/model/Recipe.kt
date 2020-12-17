ef996593b49399f017f5ffb30a8f1444477e3f17
Mustafa Afandi <tapaafandi@gmail.com>
2020-12-18 01:15:03 +0700

    Core Business Model

---
+++ app/src/main/java/com/tapaafandi/mvvmrecipeapplication/domain/model/Recipe.kt
@@ -0,0 +1,19 @@
+package com.tapaafandi.mvvmrecipeapplication.domain.model
+
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+data class Recipe(
+        var id: Int? = null,
+        var title: String? = null,
+        var publisher: String? = null,
+        var featuredImage: String? = null,
+        var rating: Int? = 0,
+        var sourceUrl: String? = null,
+        var description: String? = null,
+        var cookingInstructions: String? = null,
+        var ingredients: List<String>? = null,
+        var dateAdded: String? = null,
+        var dateUpdated: String? = null,
+): Parcelable
