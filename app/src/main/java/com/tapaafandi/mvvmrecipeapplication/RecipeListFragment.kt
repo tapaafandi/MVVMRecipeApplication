c2839af7271e5c6e397a753179eb17c47fe3baec
Mustafa Afandi <tapaafandi@gmail.com>
2020-12-18 21:00:37 +0700

    added fragment

---
+++ app/src/main/java/com/tapaafandi/mvvmrecipeapplication/RecipeListFragment.kt
@@ -0,0 +1,47 @@
+package com.tapaafandi.mvvmrecipeapplication
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Button
+import androidx.compose.material.Text
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.dp
+import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+
+abstract class RecipeListFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return ComposeView(requireContext()).apply {
+            setContent {
+                Column(modifier = Modifier.padding(16.dp)) {
+                    Text(
+                        text = "Recipe List Fragment",
+                        style = TextStyle(
+                            fontSize = TextUnit.Companion.Sp(21)
+                        )
+                    )
+                    Spacer(modifier = Modifier.padding(10.dp))
+                    Button(onClick = {
+                        findNavController().navigate(R.id.viewRecipe)
+                    }) {
+                        Text(text = "TO RECIPE FRAGMENT")
+                    }
+                }
+
+            }
+        }
+    }
+}
