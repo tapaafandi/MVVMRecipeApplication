dab5e5e22b0b61f404c32e323e2b93166ce91ad9
Mustafa Afandi <tapaafandi@gmail.com>
2020-12-25 11:58:01 +0700

    adding searchview toolbar

--- app/src/main/java/com/tapaafandi/mvvmrecipeapplication/presentation/ui/recipe_list/RecipeListFragment.kt
+++ app/src/main/java/com/tapaafandi/mvvmrecipeapplication/presentation/ui/recipe_list/RecipeListFragment.kt
@@ -1,32 +1,24 @@
 package com.tapaafandi.mvvmrecipeapplication.presentation.ui.recipe_list
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material.Button
-import androidx.compose.material.Text
-import androidx.compose.material.TextField
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.savedinstancestate.savedInstanceState
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.viewinterop.viewModel
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.navigation.findNavController
-import com.tapaafandi.mvvmrecipeapplication.R
 import com.tapaafandi.mvvmrecipeapplication.presentation.components.RecipeCard
-import com.tapaafandi.mvvmrecipeapplication.util.TAG
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -47,23 +39,52 @@ class RecipeListFragment : Fragment() {
                 val query = viewModel.query.value
 
                 Column {
-                    TextField(
-                            value = query,
-                            onValueChange = { newValue ->
-                                viewModel.onQueryChanged(newValue)
-                            }
-                    )
-                    Spacer(modifier = Modifier.padding(10.dp))
+                    Surface(
+                            modifier = Modifier
+                                    .fillMaxWidth(),
+                            color = MaterialTheme.colors.primary,
+                            elevation = 8.dp
+                    ) {
+                        Row(
+                                modifier = Modifier.fillMaxWidth()
+                        ) {
+                            TextField(
+                                    modifier = Modifier
+                                            .fillMaxWidth(0.9f)
+                                            .padding(8.dp),
+                                    value = query,
+                                    onValueChange = { newValue ->
+                                        viewModel.onQueryChanged(newValue)
+                                    },
+                                    label = {
+                                        Text(text = "Search")
+                                    },
+                                    keyboardOptions = KeyboardOptions(
+                                            keyboardType = KeyboardType.Text,
+                                            imeAction = ImeAction.Search
+                                    ),
+                                    leadingIcon = {
+                                        Icon(Icons.Filled.Search)
+                                    },
+                                    onImeActionPerformed = { action, softKeyboardController ->
+                                        if (action == ImeAction.Search) {
+                                            viewModel.newSearch(query)
+                                            softKeyboardController?.hideSoftwareKeyboard()
+                                        }
+                                    },
+                                    textStyle = TextStyle(color = MaterialTheme.colors.onSurface),
+                                    backgroundColor = MaterialTheme.colors.surface,
+                            )
+                        }
+                    }
                     LazyColumn {
                         itemsIndexed(
                                 items = recipes
                         ){ index, recipe ->
                             RecipeCard(recipe = recipe, onClick = {})
-
                         }
                     }
                 }
-
 
             }
         }
--- app/src/main/java/com/tapaafandi/mvvmrecipeapplication/presentation/ui/recipe_list/RecipeListViewModel.kt
+++ app/src/main/java/com/tapaafandi/mvvmrecipeapplication/presentation/ui/recipe_list/RecipeListViewModel.kt
@@ -24,15 +24,15 @@ constructor(
     val query = mutableStateOf("Chicken")
 
     init {
-        newSearch()
+        newSearch(query.value)
     }
 
-    private fun newSearch() {
+    fun newSearch(query: String) {
         viewModelScope.launch {
             val result = repository.search(
                 token = token,
                 page = 1,
-                query = "chicken"
+                query = query
             )
             recipes.value = result
         }
