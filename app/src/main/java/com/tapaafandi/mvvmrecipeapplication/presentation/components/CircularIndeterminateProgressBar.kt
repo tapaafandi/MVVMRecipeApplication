788a658e3ee980162b48496c78edccfc4bad729c
Mustafa Afandi <tapaafandi@gmail.com>
2021-01-02 19:20:51 +0700

    progress bar with constraint

--- app/src/main/java/com/tapaafandi/mvvmrecipeapplication/presentation/components/CircularIndeterminateProgressBar.kt
+++ app/src/main/java/com/tapaafandi/mvvmrecipeapplication/presentation/components/CircularIndeterminateProgressBar.kt
@@ -3,12 +3,8 @@ package com.tapaafandi.mvvmrecipeapplication.presentation.components
 import androidx.compose.foundation.layout.ConstraintLayout
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.CircularProgressIndicator
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.unit.TextUnit
 
 @Composable
 fun CircularIndeterminateProgressBar(
@@ -18,23 +14,13 @@ fun CircularIndeterminateProgressBar(
         ConstraintLayout(
                 modifier = Modifier.fillMaxSize()
         ) {
-            val (progressBar, text) = createRefs()
+
+            val (progressBar) = createRefs()
+            val topGuideLine = createGuidelineFromTop(0.3f)
+
             CircularProgressIndicator(
                     modifier = Modifier.constrainAs(progressBar) {
-                        top.linkTo(parent.top)
-                        bottom.linkTo(parent.bottom)
-                        start.linkTo(parent.start)
-                        end.linkTo(parent.end)
-                    }
-            )
-            Text(
-                    text = "Loading...",
-                    style = TextStyle(
-                            color = Color.Black,
-                            fontSize = TextUnit.Companion.Sp(15)
-                    ),
-                    modifier = Modifier.constrainAs(text) {
-                        top.linkTo(progressBar.bottom)
+                        top.linkTo(topGuideLine)
                         start.linkTo(parent.start)
                         end.linkTo(parent.end)
                     }
